4d7059b57f4104f93db44d59c713f4579619b625
jwjung <jjwonyop@gmail.com>
2018-09-27 22:36:46 +0900

    JPQL

--- src/main/java/com/jwjung/demospringdata/JpaRunner.java
+++ src/main/java/com/jwjung/demospringdata/JpaRunner.java
@@ -5,9 +5,7 @@
 
 package com.jwjung.demospringdata;
 
-import com.jwjung.demospringdata.entity.Comment;
 import com.jwjung.demospringdata.entity.Post;
-import org.hibernate.Session;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
@@ -15,6 +13,11 @@ import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import java.util.List;
 
 @Component
 public class JpaRunner implements ApplicationRunner {
@@ -49,19 +52,48 @@ public class JpaRunner implements ApplicationRunner {
 //
 //        post.addComment(comment1);
 
-        Session session = entityManager.unwrap(Session.class);
-        Post post = session.get(Post.class, 4L);
-        System.out.println("==============");
-        System.out.println(post.getTitle());
-        post.getComment().forEach(str -> {
-            System.out.println("===================");
-            System.out.println(str.getComment());
-            System.out.println("===================");
-        });
-//        session.save(post);
-        Comment comment = session.get(Comment.class, 5L);
-        System.out.println("=======================");
-        System.out.println(comment.getPost().getTitle());
-        System.out.println(comment.getComment());
+//        Session session = entityManager.unwrap(Session.class);
+//        Post post = session.get(Post.class, 4L);
+//        System.out.println("==============");
+//        System.out.println(post.getTitle());
+//        post.getComment().forEach(str -> {
+//            System.out.println("===================");
+//            System.out.println(str.getComment());
+//            System.out.println("===================");
+//        });
+////        session.save(post);
+//        Comment comment = session.get(Comment.class, 5L);
+//        System.out.println("=======================");
+//        System.out.println(comment.getPost().getTitle());
+//        System.out.println(comment.getComment());
+
+        /**
+         * JPQL(HQL)
+         * Entity 기준 작성 쿼리
+         */
+        TypedQuery<Post> query = (TypedQuery<Post>) entityManager.createQuery("SELECT p from Post as p");
+        List<Post> posts = query.getResultList();
+        posts.forEach(System.out::println);
+
+        System.out.println("==========================================");
+
+        /**
+         * Criteria Query(type safe Query)
+         */
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Post> builderQuery = criteriaBuilder.createQuery(Post.class);
+        Root<Post> root = builderQuery.from(Post.class);
+        builderQuery.select(root);
+
+        List<Post> resultList = entityManager.createQuery(builderQuery).getResultList();
+        resultList.forEach(System.out::println);
+        System.out.println("==========================================");
+
+        /**
+         * Native Query
+         */
+        List<Post> posts1 = entityManager.createNativeQuery("SELECT * from post", Post.class).getResultList();
+        posts1.forEach(System.out::println);
+
     }
 }
